e8502d8c31dd711cbe9781a42b35b1d114cd490d
Naukite <naukite@gmail.com>
2023-06-03 18:39:01 +0100

    feat: incorrecta la instalacion en ubuntu. se elimina

--- docker_install_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install docker -y
-sudo systemctl docker start
-sudo usermod -a -G docker $USER
-newgrp docker
-sudo chkconfig docker on
-sudo systemctl status docker.service
